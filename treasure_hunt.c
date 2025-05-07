ab80c3454843dd3e4774abc586ad92b33b74cf1d
tuktuk24 <82012275+tuktuk24@users.noreply.github.com>
2025-05-07 11:57:58 +0530

    Create treasure_hunt.c

---
+++ treasure_hunt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<malloc.h>
+long long solve (int n, int* nums) {
+    // Write your code here
+
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int i_nums;
+    int *nums = (int *)malloc(sizeof(int)*(n));
+    for(i_nums = 0; i_nums < n; i_nums++)
+    	scanf("%d", &nums[i_nums]);
+
+    long long out_ = solve(n, nums);
+    printf("%lld", out_);
+}
